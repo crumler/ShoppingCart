6786f591aedfe808f33d9ae1e463b3b0184f950e
Chris Rumler <cgrumler@gmail.com>
2021-01-28 14:56:55 -0500

    added cart layout

--- script.js
+++ script.js
@@ -93,8 +93,6 @@ function setItems(product) {
 
 //! Function that calculates total cost of product(s) added to cart
 function totalCost(product) {
-    console.log("The product price is ", product.price);
-
     let cartCost = localStorage.getItem("totalCost");
     
     if (cartCost != null) {
@@ -105,4 +103,43 @@ function totalCost(product) {
     }
 };
 
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem("totalCost");
+
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = "";
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="./images/${item.tag}.jpg" />
+                <span>${item.name}</span>
+            </div>
+            <div class="price">$${item.price}.00</div>
+            <div class="quantity">
+                <ion-icon name="remove-circle"></ion-icon>
+                <span>${item.inCart}</span>
+
+                <ion-icon name="add-circle"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price}.00
+            </div>
+            `;
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">Basket Total</h4>
+                <h4 class="basketTotal">$${cartCost}.00</h4>
+            </div>
+        `;
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
